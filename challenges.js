d523aa600736c691d54b843a946ae24dd0fdeaa5
dsl2022 <2015rpro@gmail.com>
2023-03-30 22:10:52 -0400

    first commit

---
+++ challenges.js
@@ -0,0 +1,21 @@
+// Create a variable called myName and assign it your name as a string.
+
+// Create an array called numbers and add five numbers to it.
+
+// Create a variable called myAge and assign it your age as a number.
+
+// Create an object called person with properties name, age, and gender.
+
+// Create a variable called isRaining and assign it a boolean value based on whether it is currently raining or not.
+
+// Create a function called greet that takes a name as a parameter and returns a greeting message using template literals.
+
+// Create a function called multiply that takes two numbers as parameters and returns their product.
+
+// Create a function called divide that takes two numbers as parameters and returns their quotient.
+
+// BONUS
+// Create a function called isEven that takes a number as a parameter and returns true if the number is even, false otherwise.
+
+
+
